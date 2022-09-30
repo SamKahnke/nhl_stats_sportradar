480f346c70e5464f7d82ce288c9cc4772afb06b6
User <user@Users-MacBook-Pro.local>
2022-09-30 16:01:12 -0500

    refactor db name to config

--- postgresql.js
+++ postgresql.js
@@ -2,12 +2,13 @@ import postgresql from 'pg';
 import os from 'os';
 
 const { Pool } = postgresql;
+const config = require('config');
 
 export default (callback = null) => {
   // NOTE: PostgreSQL creates a superuser by default on localhost using the OS username.
   const pool = new Pool({
     user: process.env.NODE_ENV === 'development' && (os.userInfo() || {}).username || '',
-    database: 'nhl_stats',
+    database: config.get('database.database'),
     password: '',
     host: '127.0.0.1',
     port: 5000,
